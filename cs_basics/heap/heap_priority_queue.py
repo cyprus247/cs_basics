30347975910121b88c95cdd87cb95c5338f4870a
Ciprian Dirnu <cip_dirnu@gmail.com>
2022-07-13 13:43:22 +0200

    allow heap to be initialised with a sequence

--- cs_basics/heap/heap_priority_queue.py
+++ cs_basics/heap/heap_priority_queue.py
@@ -1,10 +1,11 @@
 from cs_basics.priority_queue.priority_queue_base import PriorityQueueBase
 
+
 class HeapPriorityQueue(PriorityQueueBase):
     """A min-oriented priority queue implemented with a binary heap"""
 
     def _parent(self, j):
-        return (j-1) // 2
+        return (j - 1) // 2
 
     def _left(self, j):
         return j * 2 + 1
@@ -40,10 +41,17 @@ class HeapPriorityQueue(PriorityQueueBase):
                 self._swap(j, smallest)
                 self._downheap(smallest)
 
+    def _heapify(self):
+        start = self._parent(len(self) - 1)
+        for j in range(start, -1, -1):
+            self._downheap(j)
+
     # ---------------------- public behaviours ----------------------
 
-    def __init__(self):
-        self._data = []
+    def __init__(self, contents=()):
+        self._data = [self._Item(k, v) for k, v in contents]
+        if len(self._data) > 1:
+            self._heapify()
 
     def __len__(self):
         return len(self._data)
@@ -67,3 +75,14 @@ class HeapPriorityQueue(PriorityQueueBase):
         item = self._data.pop()  # remove min from list
         self._downheap(0)  # bubble down
         return (item._key, item._value)
+
+
+if __name__ == '__main__':
+    rand_list = [(312, 'jack'), (425, 'jill'), (253, 'bob'), (6457, 'jan'), (2523, 'stan'), (856, 'jane'),
+                 (23, 'lilith'), (735, 'stella'), (253, 'maggie'), (999, 'ella')]
+    h = HeapPriorityQueue(rand_list)
+    print(h.remove_min())
+    print(h.remove_min())
+    print(h.remove_min())
+    print(h.remove_min())
+    print(h.remove_min())
